4001c31a999743784569106088d54c04fa6047a0
rajeevsagar <rajeev22335@gmail.com>
2022-03-26 12:28:57 +0000

    Debugging Simple Todos tests

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import SimpleTodos from './Components/SimpleTodos'
+
+import './App.css'
+
+const App = () => <SimpleTodos />
+
+export default App
---
+++ src/Components/TodoItem/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+const TodoItem = props => {
+  const {todoDetails, deleteTodo} = props
+  const {id, title} = todoDetails
+
+  const onDeleteTodo = () => {
+    deleteTodo(id)
+  }
+
+  return (
+    // FIX:5 only single element will return on every function adding Fragment
+    <li className="todo-item">
+      <p className="title">{title}</p>
+      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
